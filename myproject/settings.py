b04c93d07c40accdcafff6d3f8a49231e8038416
guadalupeturri <magaliturri@gmail.com>
2026-04-04 21:49:51 -0300

    Creación de un superusuario

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+    'blog',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
